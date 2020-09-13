89f40bb80e28847b6b9ed606fcc1db5b64429cd4
Jeison <jeajimenezbl@qqunal.edu.co>
2020-09-13 18:11:49 -0500

    Primer punto del Modelo SIR con gamma 0.08 y beta 0.35. Se acoplo la ecuación para r.

--- Talleres_2020-II/Taller1/Modelo_SIR.cpp
+++ Talleres_2020-II/Taller1/Modelo_SIR.cpp
@@ -1,60 +1,71 @@
-//Programa que implementa el Metodo de Runge-Kutt-4 para integrar una ecuacion diferencia acoplada
-
-#include <iostream> //incluye la libreria iostream permite recibir datos del teclado y sacarla por pantella
+#include <iostream>
 #include <cmath>
-using namespace std; // espacio de nombres. Se va a utilizar el standar.
 
-const double Beta=0.35;
-const double Gamma=0.08;
-//funcion
 
-///////////////////////////7////////////////////////////////////////////////////////7
+using namespace std;
 
-double f1(double t, double S, double I){
-  return -Beta*S*I;
-  }
+const double beta = 0.35;
+const double gama= 0.08;
+const double tau=1/gama;
 
-double f2(double t, double S, double I){
-  return Beta*S*I-Gamma*I;
-  }
-void UnPasoRK4(double & t, double & S, double & I, double dt){//PAso por referencia se agrega & a la variable
-  double dS1, dS2,dS3,dS4; double dI1, dI2,dI3,dI4;
-  
-  dS1=dt*f1(t,S,I);      dI1=dt*f2(t,S,I);
-
-  
-  dS2=dt*f1(t+dt*0.5,S+dS1*0.5,I+dI1*0.5); dI2=dt*f2(t+dt*0.5,S+dS1*0.5,I+dI1*0.5);
-  
-  dS3=dt*f1(t+dt*0.5,S+dS2*0.5,I+dI2*0.5); dI3=dt*f2(t+dt*0.5,S+dS2*0.5,I+dI2*0.5);
-  
-  dS4=dt*f1(t+dt*0.5,S+dS3*0.5,I+dI3*0.5); dI4=dt*f2(t+dt*0.5,S+dS3*0.5,I+dI3*0.5);
-  
-  S+=1/6.0*(dS1+2*(dS2+dS3)+dS4);
-  I+=1/6.0*(dI1+2*(dI2+dI3)+dI4);
-  t+=dt;
+double F1(double t, double s, double i) // derivada de s respecto a t
+{
+  return -beta*s*i;
 }
 
-//////////////////////////////////7////////////////////////////////////////////////////
-//%%%%%%%%MAIN
-
-int main(void)
+double F2(double t, double s, double i) // derivada de i respecto a t
 {
-  double t,S,I,r;
-  double dt=1.0,tfinal=120.0;
+  return beta*s*i-gama*i;
+}
 
-  //Valores iniciales
-  S=0.999;
-  I=0.001;
+double F3(double t, double i) //derivada de r respecto a t
+{
+  return gama*i;
+}
+
+void Paso_RungeKutta(double t, double &s, double &i,double r, double dt)
+{
+  double ds1,di1,dr1,ds2,di2,dr2,ds3,di3,dr3,ds4,di4,dr4,ds,di,dr;
+  ds1=dt*F1(t,s,i);
+  di1=dt*F2(t,s,i);
+  //dr1=dt*F3(t,i);
   
+  ds2=dt*F1(t+0.5*dt,s+0.5*ds1,i+0.5*di1);
+  di2=dt*F2(t+0.5*dt,s+0.5*ds1,i+0.5*di1);
+  //dr2=dt*F3(t+0.5*dt,i+0.5*di1);
+
+  ds3=dt*F1(t+0.5*dt,s+0.5*ds2,i+0.5*di2);
+  di3=dt*F2(t+0.5*dt,s+0.5*ds2,i+0.5*di2);
+  //dr3=dt*F3(t+0.5*dt,i+0.5*di2);
+
+  ds4=dt*F1(t+dt,s+ds3,i+di3);
+  di4=dt*F2(t+dt,s+ds3,i+di3);
+  //dr4=dt*F3(t+dt,i+di3);
+
+  ds=(ds1+2*ds2+2*ds3+ds4)/6;
+  di=(di1+2*di2+2*di3+di4)/6;
+  //dr=(dr1+2*dr2+2*di3+di4)/6;
+  s+=ds;
+  i+=di;
+  //r+=dr;
+}
+
+int main()
+{
+  double s,i,r,t,t0,dt;
+  int T;
+  s=0.999;
+  i=0.001;
   
-  //
+  t0=0;
+  dt=0.1;
+  T=120;
+  for(t=t0;t<=T;t+=dt)
+  {
+    r=1-s-i;
+    cout<<t<<"\t"<<s<<"\t"<<i<<"\t"<<r<<endl;
+    Paso_RungeKutta(t,s,i,r,dt);
+    }
 
-  for(t=0;t<tfinal;)
-    {
-      r=1.0-S-I;
-      cout<<t<<" "<<S<<" "<<I<<" "<<r<<endl;
-      UnPasoRK4(t,S,I,dt);
-    } 
-
-  return 0; //Retorna 0 para avisar que todo funciono bien
+  return 0;
 }
